a2efcd9e64346da2aa7a3a9d2a6e19f1f34dde64
hojun <dlghwns2405@gmail.com>
2023-06-22 18:08:46 +0900

    문제

---
+++ RoadBook.CsharpBasic.Chatper06/Examples/Q02.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter06.Examples
+{
+    internal class Q02
+    {
+        public void Run()
+        {
+            int[] arr = new int[5];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("숫자를 입력해주세요:");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+             
+            Array.Sort(arr);
+
+            Console.WriteLine("오름차순 정렬 결과 {0} ,{1},{2},{3},{4}", arr[0], arr[1], arr[2], arr[3], arr[4]  );
+        }
+    }
+}
--- RoadBook.CsharpBasic.Chatper06/Program.cs
+++ RoadBook.CsharpBasic.Chatper06/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Examples.Q04 ex001 = new Examples.Q04();
+            Examples.Q02 ex001 = new Examples.Q02();
             ex001.Run();
         }
     }
