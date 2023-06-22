9a1dd19184e006f429e1e77d5f3e74f3b7ea921e
hojun <dlghwns2405@gmail.com>
2023-06-22 17:41:16 +0900

    문제

--- RoadBook.CsharpBasic.Chatper06/Examples/Q04.cs
+++ RoadBook.CsharpBasic.Chatper06/Examples/Q04.cs
@@ -17,15 +17,11 @@ namespace RoadBook.CsharpBasic.Chapter06.Examples
             List<string> list = new List<string>();
             List<string> list1 = new List<string>();
 
-            list1.Add("일본");
-            list1.Add("브라질");
-            list1.Add("독일");
-            list1.Add("스페인");
+            list1.Add(" \n 일본 \n 브라질 \n 독일 \n 스페인");
+           
 
-            list.Add("대한민국");
-            list.Add("프랑스");
-            list.Add("미국");
-            list.Add("이집트");
+            list.Add(" \n 대한민국 \n 프랑스 \n 미국 \n 이집트");
+            
 
             dict.Add("A", list);
             dict.Add("H", list1);
@@ -41,7 +37,7 @@ namespace RoadBook.CsharpBasic.Chapter06.Examples
                 }
                 if (dict.ContainsKey(word)) 
                 {
-                    Console.WriteLine("{0}에 속한 나라는 : {1}", word, dict[word]);
+                    Console.WriteLine("{0} 조 에 속한 나라는 : {1}", word, dict[word][0]);
                 }
             }
         }
