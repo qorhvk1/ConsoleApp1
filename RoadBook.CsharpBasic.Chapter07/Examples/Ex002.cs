2c037a5266bb9b5764467b90e6c11465e7f65655
sive331 <80349847+sive331@users.noreply.github.com>
2023-06-30 00:00:33 +0900

    chapter07

---
+++ RoadBook.CsharpBasic.Chapter07/Examples/Ex002.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter07.Examples
+{
+    internal class Ex002
+    {
+        public void Run()
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Hi");
+            Console.WriteLine(list[0]);
+            
+            list.Clear();
+            Console.WriteLine(list[0]);
+
+        }
+    }
+}
---
+++ RoadBook.CsharpBasic.Chapter07/Examples/Ex004.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter07.Examples
+{
+    internal class Ex004
+    {
+        public void Run()
+        {
+            Console.WriteLine("숫자를 입력하세요 :");
+
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("입력된 숫자는 {0}", number);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(" 예외 상황 발생, 관리자에게 문의");
+                Console.WriteLine("에러코드 : {0}",e.HResult);
+                Console.WriteLine("에러 메세지 : {0}", e.Message);
+            }
+        }
+    }
+}
