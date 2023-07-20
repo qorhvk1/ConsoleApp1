3c39884986b0cf749a8a4db52395baded2b4fa5e
hojun <dlghwns2405@gmail.com>
2023-07-21 04:59:23 +0900

    08

--- RoadBook.CsharpBasic.Chatper08/Model/User.cs
+++ RoadBook.CsharpBasic.Chatper08/Model/User.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RoadBook.CsharpBasic.Chatper08.Model
+namespace RoadBook.CsharpBasic.Chapter08.Model
 {
     internal class User
     {
         public string userID { get; set; }
         public string Name { get; set; }
-        public string Age { get; set; }
+        public int Age { get; set; }
 
         public string Job { get; set; }
 
